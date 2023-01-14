4561465d3067a7563b3232b23ab5892f157a6992
Per Helge Vollmo <per.helge.vollmo@gmail.com>
2023-01-14 23:01:16 +0100

    24 Add lines to all pages

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ for index, row in df.iterrows():
     pdf.set_font('Times', style='B', size=24)
     pdf.set_text_color(100, 100, 100) # = grey (RGB)
     pdf.cell(w=0, h=24, txt=row['Topic'], align='L', ln=1, border=0)
-    pdf.line(10, 28, 200, 28) # x1, y1, x2, y2)
+    for y in range(27, 298, 10):
+        pdf.line(10, y, 200, y)
+
 
     # Set the footer
     pdf.ln(250) # Add 278 breaklines on each page - to the btm of page.
@@ -31,6 +33,13 @@ for index, row in df.iterrows():
         pdf.set_font('Times', style='I', size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row['Topic'], align='R', ln=1, border =0)
+        for y in range(16, 298, 10):
+            pdf.line(10, y, 200, y)
+
+       
+
+    
+
 
 
 pdf.output('output.pdf')
